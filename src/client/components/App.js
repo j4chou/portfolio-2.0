4fc8431ffc57384a3c9f509a6477f2e41f8734a0
Jessica Chou <j4chou@gmail.com>
2017-10-22 21:49:15 -0700

    Organize folder structure.

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './../styles/app.scss';
+
+import Footer from './Footer/Footer';
 
 class App extends Component {
   render() {
@@ -13,6 +16,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Footer />
       </div>
     );
   }
