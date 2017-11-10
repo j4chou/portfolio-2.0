613e67e29a79e79847b5e9eddf5641dd143eb93d
Jessica Chou <j4chou@gmail.com>
2017-11-09 21:21:22 -0800

    Added links and styling.

--- src/client/components/About/About.js
+++ src/client/components/About/About.js
@@ -9,7 +9,9 @@ import './../../styles/app.css';
 let Element = Scroll.Element;
 
 class About extends Component {
+
   render() {
+    
     return (
       <Element name="about" className="about section">
         <div className="title">
@@ -17,12 +19,16 @@ class About extends Component {
           <p>Software Engineer</p>
           <p>Los Angeles, CA</p>
           <span class="icon-row">
-            <i class="fa fa-linkedin-square"></i>
-            <i class="fa fa-github-square"></i>
+            <a href="http://www.linkedin.com/in/jessicachou1/" key="linkedin">
+              <i class="fa fa-linkedin-square"></i>
+            </a>
+            <a href="http://www.github.com/j4chou" key="github">
+              <i class="fa fa-github-square"></i>
+            </a>
           </span>
         </div>
         <div className="body">
-          This is the body
+          <p>This is the body</p>
         </div>
       </Element>
     )
